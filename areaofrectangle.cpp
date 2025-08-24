ac70860e3b5cecfc1681d9a57aa951a0a36865ae
Megha-Purushan <meghapurushan88@gmail.com>
2025-08-24 20:40:00 +0530

    Add files via upload

---
+++ areaofrectangle.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int l,b,a;
+    cout<<"Enter length of the rectangle:";
+    cin>>l;
+    cout<<"Enter breadth of the rectangle:";
+    cin>>b;
+    a=(l*b);
+    cout<<"Area of the rectangle is "<<a;
+    return 0;
+}
