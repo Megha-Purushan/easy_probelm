70cb185e8049f5e59eb12dbfe40965e414e780dd
Megha-Purushan <meghapurushan88@gmail.com>
2025-08-24 20:45:12 +0530

    Add files via upload

---
+++ sum.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+ int a,b,sum=0;
+
+ cout<<"Enter number 1:";
+ cin>>a;
+ cout<<"Enter number 2:";
+ cin>>b;
+ sum=a+b;
+ cout<<"Sum of these two number is "<<sum;
+ return 0;
+}
