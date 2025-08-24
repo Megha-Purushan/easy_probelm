1274f0fb94bd13ede49957ae8dcaf95124e19738
Megha-Purushan <meghapurushan88@gmail.com>
2025-08-24 20:40:55 +0530

    Add files via upload

---
+++ areaofrectangle2.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int l=10,b=2,a;
+
+    a=(l*b);
+    cout<<"Area of the rectangle is "<<a;
+    return 0;
+}
---
+++ avg.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a,b,c,avg;
+    cout<<"Enter number 1:";
+    cin>>a;
+    cout<<"Enter number 2:";
+    cin>>b;
+    cout<<"Enter number 3:";
+    cin>>c;
+    avg=(a + b + c)/3;
+    cout<<"Average of the number: "<<avg;
+    return 0;
+}
