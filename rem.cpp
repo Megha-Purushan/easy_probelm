bc735247b1fefb91b2c6d625781837f9c8856219
Megha-Purushan <meghapurushan88@gmail.com>
2025-08-24 20:43:05 +0530

    Add files via upload

---
+++ rem.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n,d,r;
+    //int q;
+    cout<<"Enter the number to be divident:";
+    cin>>n;
+    cout<<"Enter the divisor:";
+    cin>>d;
+//    q=n/d; //Quotient
+   // r=n%d; //remainder
+    cout<<"Remainder is "<<r;
+    //cout<<" Quotient is "<<q;
+    return 0;
+}
